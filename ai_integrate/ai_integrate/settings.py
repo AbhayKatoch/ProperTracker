cdc5643ce4512fb9a4880290c1ba8c248303e0f9
AbhayKatoch <abhaykatoch210@gmail.com>
2025-08-18 01:11:07 +0530

    final

--- ai_integrate/ai_integrate/settings.py
+++ ai_integrate/ai_integrate/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x$ahk6sr650y6$c)z12)1h9#-c$s0wbqk-=f1#3db^%$lrsuhg'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,7 +32,8 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "deb8cced37a1.ngrok-free.app",
     "127.0.0.1:8000",
-    "127.0.0.1"
+    "127.0.0.1",
+    "*",
 ]
 
 APPEND_SLASH = False
@@ -129,8 +130,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
